85e75188cb9cfdc2e592e1b6d2f458057146beda
MihalevD <mihalev.denislav@gmail.com>
2021-11-27 15:06:23 +0200

    Removed static?

--- server/index.js
+++ server/index.js
@@ -8,8 +8,6 @@ const PORT = 3000;
 const app = express();
 app.use(cors(), helmet())
 
-app.use("/static", express.static("../client"));
-
 app.use("/", userController)
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
