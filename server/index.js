d09062731f5060863b10c55ae4aea00f5521fab3
Viktor Kostadinov <viktor.pts@abv.bg>
2021-12-03 13:53:50 +0200

    Base Socket.io binding

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { Server } from "socket.io";
+import * as http from "http";
 import userController from "./controllers/userController.js";
+
 
 const PORT = 5000;
 
 const app = express();
+const server = http.createServer(app);
+
 app.use(cors(), helmet());
 app.use(express.json());
-
 app.use("/users", userController);
-app.listen(PORT, () => {
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {  console.log("a user connected");});
+
+
+server.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
