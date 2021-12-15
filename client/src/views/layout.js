6398f26dc02f458881b761a3e34699358d0ed659
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2021-12-15 11:11:52 +0200

    Updated the navigation bar styles.

--- client/src/views/layout.js
+++ client/src/views/layout.js
@@ -2,16 +2,31 @@ import { html } from "../lib.js";
 
 
 export const layoutTemplate = (state, view) => html`
-<nav>
-    <a href="/">Home</a>
-    <a href="/rooms">Rooms</a>
+<header class="site-header">
+    <div class="wrapper">
+        <div class="site-branding">
+            <a href="/">
+                <p class="site-title">
+                    Chess
+                </p>
+                <p class="site-subtitle">
+                    Online
+                </p>
+            </a>
+        </div>
 
-    ${state.user ? 
-        html`<a href="javascript:void(0)" @click=${state.onLogout}>Logout</a>` : 
-        html`
-        <a href="/login">Login</a>
-        <a href="/register">Register</a>
-    `}
-</nav>
+        <nav class="site-nav">
+            <ul>
+                <li><a href="/rooms"><i class="fas fa-chess"></i> Rooms</a></li>
+                ${state.user ? 
+                    html`<li><a href="javascript:void(0)" @click=${state.onLogout}><i class="fas fa-sign-out-alt"></i> Logout</a></li>` : 
+                    html`
+                    <li><a href="/login"><i class="fas fa-sign-in-alt"></i> Login</a></li>
+                    <li><a href="/register"><i class="fas fa-user-plus"></i> Register</a></li>
+                `}
+            </ul>
+        </nav>
+    </div>
+</header>
 ${view}
 <footer>&copy; 2021</footer>`;
