0671f9ba04fd56bb9386c20b85cbfa3c9d0d755c
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2022-01-27 11:52:27 +0200

    Fixed a problem in the navigation bar.

--- client/src/views/layout.js
+++ client/src/views/layout.js
@@ -16,15 +16,18 @@ export const layoutTemplate = (state, view) => html`
         </div>
 
         <nav class="site-nav">
-            <ul>
-                <li><a href="/rooms"><i class="fas fa-chess"></i> Rooms</a></li>
-                ${state.user ? 
-                    html`<li><a href="javascript:void(0)" title="Logout user" @click=${state.onLogout}><i class="fas fa-sign-out-alt"></i> ${state.user.username}</a></li>` : 
-                    html`
+            ${state.user ? 
+                html`
+                <ul class="logged">
+                    <li><a href="/rooms"><i class="fas fa-chess"></i> Rooms</a></li>
+                    <li><a href="javascript:void(0)" title="Logout user" @click=${state.onLogout}><i class="fas fa-sign-out-alt"></i> ${state.user.username}</a></li>
+                </ul>` : 
+                html`
+                <ul>
+                    <li><a href="/rooms"><i class="fas fa-chess"></i> Rooms</a></li>
                     <li><a href="/login"><i class="fas fa-sign-in-alt"></i> Login</a></li>
                     <li><a href="/register"><i class="fas fa-user-plus"></i> Register</a></li>
-                `}
-            </ul>
+                </ul>`}
         </nav>
     </div>
 </header>
