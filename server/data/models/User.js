8a292d1e1c85d94a12e59811cbab5c5806189312
Viktor Kostadinov <viktor.pts@abv.bg>
2022-02-09 10:07:47 +0200

    Added case-insensitive index to username

--- server/data/models/User.js
+++ server/data/models/User.js
@@ -6,4 +6,12 @@ const schema = new mongoose.Schema({
     hashedPassword: { type: String, required: true }
 });
 
+schema.index({ username: 1}, {
+    unique: true,
+    collation: {
+        locale: "en",
+        strength: 2
+    }
+});
+
 export default mongoose.model("User", schema);
